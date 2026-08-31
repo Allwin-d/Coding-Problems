810feb1ad27b347be38dd7299ec2de5b1c9bb8e7
Allwin <allwinselva7@gmail.com>
2026-08-31 22:51:24 +0530

    fixed LongestString without repeating character problem

---
+++ Strings/LongestStringWithoutRepeatingCharacters.ts
@@ -0,0 +1,23 @@
+const LongestStringWithoutRepeatingCharacters = (s:string)=>{
+    let set = new Set();
+    let left = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < s.length; right++) {
+
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+
+        set.add(s[right]);
+
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+};
+
+const s = "abcabcbb";
+LongestStringWithoutRepeatingCharacters(s);
+
